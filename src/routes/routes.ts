739728078f24dd38ca21c96ec01f2e4a7b63872d
claudiostocco <claudiostocco@gmail.com>
2023-04-26 18:00:00 -0300

    Initial commit

---
+++ src/routes/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+// import { complaintsRouter } from './complaints';
+import { informationsRouter } from './informations';
+
+export const routes = Router();
+
+routes.get('/', (req, res) => {
+    return res.sendFile(__dirname+'/static/index.html');
+});
+
+routes.get('/version', (req, res) => res.json({version: '1.0.0'}));
+
+// routes.use('/complaints', complaintsRouter);
+routes.use('/informations', informationsRouter);
---
+++ src/services/database/mongodb.ts
@@ -0,0 +1,44 @@
+import { Db, MongoClient } from 'mongodb';
+
+type DatabaseConnect = {
+  client: MongoClient;
+  db: Db;
+}
+
+// let uri = process.env.MONGODB_URI || "";
+// let dbName = process.env.MONGODB_DB;
+// jn5eSfgW7q10dHwb
+// mongodb+srv://pi_user:<password>@cluster0.eyy6ldk.mongodb.net/?retryWrites=true&w=majority
+
+let uri = 'mongodb+srv://api_user:jn5eSfgW7q10dHwb@cluster0.zbzxnhl.mongodb.net/';
+let dbName = 'pandemicmonitor';
+
+let cachedClient: any = null;
+let cachedDb: any = null;
+
+if (!uri) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+if (!dbName) {
+  throw new Error(
+    'Please define the MONGODB_DB environment variable inside .env.local'
+  );
+}
+
+export async function connectToDatabase(): Promise<DatabaseConnect> {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = await MongoClient.connect(uri);
+
+  const db = await client.db(dbName)
+
+  cachedClient = client
+  cachedDb = db
+
+  return { client, db }
+}
